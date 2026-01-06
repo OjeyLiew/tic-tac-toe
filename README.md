e99803d56456070c5e9cf362d0c05f2cd066dd68
Ojey Liew <249268788+OjeyLiew@users.noreply.github.com>
2026-01-06 19:50:23 +0100

    Add logic in js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tic-tac-toe
+
+3 Objects: game, gameboard, player
+
+gameboard: contains array of gameboard 9 elements
+
+player: can take a turn and place a mark on the gameboard
+
+game: check current state of the game (win?), reset to new state
---
+++ index.js
@@ -0,0 +1,88 @@
+const gameboard = (function() {
+    let gamestate = [];
+
+    return {
+        gamestate,
+    }
+})()
+
+function player(mark) {
+    function placeMark(pos) {
+        if (pos < 0 || pos > 8) {
+            console.error("Mark must be placed inside the board (0 <= x <= 8)");
+            return;
+        }
+        if (gameboard.gamestate[pos]) {
+            console.error(`On position ${pos} is already a mark ${gameboard.gamestate[pos]} placed`);
+            return;
+        }
+        gameboard.gamestate[pos] = mark;
+        return true;
+    }
+
+    return {
+        placeMark,
+    }
+}
+
+const game = (function() {
+    const player1 = player("X");
+    const player2 = player("O");
+    let currentPlayer = player1;
+    function reset(startingPlayer = player1) {
+        gameboard.gamestate = [];
+        currentPlayer = startingPlayer;
+    }
+
+    function winner() {
+        let gamestate = gameboard.gamestate;
+        if (gamestate[0] && ((gamestate[0] === gamestate[1] && gamestate[0] === gamestate[2])
+        || (gamestate[0] === gamestate[3] && gamestate[0] === gamestate[6]))) {
+            return gamestate[0];
+        }
+        else if (gamestate[8] && ((gamestate[8] === gamestate[6] && gamestate[8] === gamestate[7])
+        || (gamestate[8] === gamestate[2] && gamestate[8] === gamestate[5]))) {
+            return gamestate[8];
+        }
+        else if (gamestate[4] && ((gamestate[4] === gamestate[0] && gamestate[4] == gamestate[8])
+        || (gamestate[4] === gamestate[1] && gamestate[4] === gamestate[7])
+        || (gamestate[4] === gamestate[2] && gamestate[4] === gamestate[6])
+        || (gamestate[4] === gamestate[3] && gamestate[4] === gamestate[5]))) {
+            return gamestate[4];
+        }
+
+        return false;
+    }
+
+    function full() {
+        for (let i = 0; i < 9; ++i) {
+            if (!gameboard.gamestate[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function tied() {
+        return !winner() && full();
+    }
+
+    function takeTurn(pos) {
+        if (winner(gameboard.gamestate)) {
+            return;
+        }
+        const succ = currentPlayer.placeMark(pos);
+        if (succ) {
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
+        }
+    }
+
+    return {
+        reset,
+        takeTurn,
+        winner,
+        tied,
+        currentPlayer,
+    }
+})()
+
